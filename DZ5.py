0503aa52bfeaebd04f1769607bf6e1f87cd08fa4
Choppa <denis.afinogenow@yandex.ru>
2022-09-15 20:58:59 +0300

    Еще одно решение

--- DZ5.py
+++ DZ5.py
@@ -21,3 +21,16 @@ print(list1)
 
 
 #ОНО НАКОНЕЦ-ТО РАБОТАЕЕЕЕТ!!!
+
+# fib = int(input('5# введите число for fib = '))
+# res_5 = []
+# for i in range(fib+1):
+#     if i==0:
+#         res_5.append(i)
+#     elif i==1:
+#         res_5.append(i)
+#         res_5.insert(0, i)
+#     else:
+#         res_5.append(res_5[len(res_5)-1]+res_5[len(res_5)-2])
+#         res_5.insert(0, (-1)**(i-1)*res_5[len(res_5)-1])
+# print(res_5)
